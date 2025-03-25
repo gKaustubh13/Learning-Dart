4b1241470a0fc67774c9642d34c9346b4912810b
gondkarKaustubh <kaustubh130104@gmail.com>
2025-03-25 20:48:15 +0530

    practice project

---
+++ bin/console_restaurant.dart
@@ -0,0 +1,5 @@
+import 'package:console_restaurant/console_restaurant.dart' as console_restaurant;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_restaurant.calculate()}!');
+}
---
+++ test/console_restaurant_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_restaurant/console_restaurant.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
